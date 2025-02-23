564641b892b805c43814565c714682992ce3b6ea
DaLLuZZ <khrushiov2012@yandex.ru>
2025-02-23 14:44:09 +0300

    HSI and UART MSP

--- src/main/io/serial.c
+++ src/main/io/serial.c
@@ -146,10 +146,15 @@ void pgResetFn_serialConfig(serialConfig_t *serialConfig)
 #ifdef USE_VCP
     if (serialConfig->portConfigs[0].identifier == SERIAL_PORT_USB_VCP) {
         serialPortConfig_t * uart1Config = serialFindPortConfiguration(SERIAL_PORT_USART1);
-        if (uart1Config && uart1Config->functionMask == 0) {
+        if (uart1Config) {
             uart1Config->functionMask = FUNCTION_MSP;
         }
     }
+#else
+	serialPortConfig_t * uart1Config = serialFindPortConfiguration(SERIAL_PORT_USART1);
+    if (uart1Config && uart1Config->functionMask == 0) {
+        uart1Config->functionMask = FUNCTION_MSP;
+	}
 #endif
 
     serialConfig->reboot_character = SETTING_REBOOT_CHARACTER_DEFAULT;
