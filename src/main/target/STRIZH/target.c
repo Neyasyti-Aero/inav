f903a8cc2e504b555dc2c0bb0c51fb03396abf1e
DaLLuZZ <khrushiov2012@yandex.ru>
2025-02-24 01:34:47 +0300

    New version pinouts

--- src/main/target/STRIZH/target.c
+++ src/main/target/STRIZH/target.c
@@ -27,19 +27,25 @@
 #include "drivers/sensor.h"
 
 timerHardware_t timerHardware[] = {
-    DEF_TIM(TIM4, CH2, PD13, TIM_USE_OUTPUT_AUTO, 0, 0), // S1
-    DEF_TIM(TIM4, CH1, PD12, TIM_USE_OUTPUT_AUTO, 0, 0), // S2
-    DEF_TIM(TIM3, CH2, PA7,  TIM_USE_OUTPUT_AUTO, 0, 0), // S3
-    DEF_TIM(TIM2, CH2, PA1,  TIM_USE_OUTPUT_AUTO, 0, 0), // S4
-    DEF_TIM(TIM8, CH4, PC9,  TIM_USE_OUTPUT_AUTO, 0, 0), // S5
-    DEF_TIM(TIM3, CH4, PB1,  TIM_USE_OUTPUT_AUTO, 0, 0), // S6
-    DEF_TIM(TIM3, CH3, PB0,  TIM_USE_OUTPUT_AUTO, 0, 0), // S7
-    DEF_TIM(TIM2, CH1, PA5,  TIM_USE_OUTPUT_AUTO, 0, 0), // S8
-    DEF_TIM(TIM10, CH1, PB8,  TIM_USE_OUTPUT_AUTO, 0, 0), // S9
-    DEF_TIM(TIM1, CH4, PE14, TIM_USE_OUTPUT_AUTO, 0, 0), // S10
-    DEF_TIM(TIM1, CH3, PE13, TIM_USE_OUTPUT_AUTO, 0, 0), // S11
-    DEF_TIM(TIM1, CH2, PE11, TIM_USE_OUTPUT_AUTO, 0, 1), // S12
-    DEF_TIM(TIM1, CH1, PE9,  TIM_USE_OUTPUT_AUTO, 0, 2), // S13
+    // DEF_TIM(TIM4, CH2, PD13, TIM_USE_OUTPUT_AUTO, 0, 0), // can be implemented in future
+    // DEF_TIM(TIM4, CH1, PD12, TIM_USE_OUTPUT_AUTO, 0, 0), // can be implemented in future
+    // DEF_TIM(TIM8, CH4, PC9,  TIM_USE_OUTPUT_AUTO, 0, 0), // can be implemented in future
+
+    // "+"-sign in comment means compatibility with older versions
+
+    DEF_TIM(TIM4, CH4, PB9,  TIM_USE_OUTPUT_AUTO, 0, 0), // S1
+    DEF_TIM(TIM14, CH1, PF9,  TIM_USE_OUTPUT_AUTO, 0, 0), // S2
+    DEF_TIM(TIM13, CH1, PF8,  TIM_USE_OUTPUT_AUTO, 0, 0), // S3
+    DEF_TIM(TIM11, CH1, PF7,  TIM_USE_OUTPUT_AUTO, 0, 0), // S4
+    DEF_TIM(TIM10, CH1, PB8,  TIM_USE_OUTPUT_AUTO, 0, 0), // S5 +
+    DEF_TIM(TIM3, CH4, PB1,  TIM_USE_OUTPUT_AUTO, 0, 0), // S6 +
+    DEF_TIM(TIM3, CH3, PB0,  TIM_USE_OUTPUT_AUTO, 0, 0), // S7 +
+    DEF_TIM(TIM3, CH2, PA7,  TIM_USE_OUTPUT_AUTO, 0, 0), // S8 +
+    DEF_TIM(TIM2, CH2, PA1,  TIM_USE_OUTPUT_AUTO, 0, 0), // S9 +
+    DEF_TIM(TIM1, CH4, PE14, TIM_USE_OUTPUT_AUTO, 0, 0), // S10 +
+    DEF_TIM(TIM1, CH3, PE13, TIM_USE_OUTPUT_AUTO, 0, 0), // S11 +
+    DEF_TIM(TIM1, CH2, PE11, TIM_USE_OUTPUT_AUTO, 0, 1), // S12 +
+    DEF_TIM(TIM1, CH1, PE9,  TIM_USE_OUTPUT_AUTO, 0, 2), // S13 +
 };
 
 const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
--- src/main/target/STRIZH/target.h
+++ src/main/target/STRIZH/target.h
@@ -19,51 +19,57 @@
 
 #define TARGET_BOARD_IDENTIFIER	"STZH"
 
-#define USBD_PRODUCT_STRING		"STRIZH"
+#define USBD_PRODUCT_STRING	"NEYASITY STRIZH FC"
 
-#define LED0                    PC14
-#define LED1                    PC15
+#define LED0			PG0
+#define LED1			PG1
 
-#define SENSORS_SET				(SENSOR_ACC|SENSOR_GYRO|SENSOR_MAG|SENSOR_BARO)
+#define SENSORS_SET		(SENSOR_ACC|SENSOR_GYRO|SENSOR_MAG|SENSOR_BARO)
 
 // *************** Gyro & ACC **********************
 #define USE_SPI
 
 #define USE_SPI_DEVICE_1
-#define SPI1_SCK_PIN            PB3
-#define SPI1_MISO_PIN           PB4
-#define SPI1_MOSI_PIN           PB5
+#define SPI1_SCK_PIN		PA5
+#define SPI1_MISO_PIN		PB4
+#define SPI1_MOSI_PIN		PB5
 
+#define USE_SPI_DEVICE_2
+#define SPI2_SCK_PIN		PB10
+#define SPI2_MISO_PIN		PC2
+#define SPI2_MOSI_PIN		PC1
 
 #define USE_IMU_BMI088
-#define IMU_BMI088_ALIGN        CW0_DEG
-#define BMI088_SPI_BUS          BUS_SPI1
-#define BMI088_GYRO_CS_PIN      PF12
-#define BMI088_GYRO_EXTI_PIN    PG6
-#define BMI088_ACC_CS_PIN       PF13
-#define BMI088_ACC_EXTI_PIN     PG8
+#define IMU_BMI088_ALIGN	CW0_DEG
+#define BMI088_SPI_BUS		BUS_SPI1
+#define BMI088_GYRO_CS_PIN	PG11
+//#define BMI088_GYRO_EXTI_PIN	PG6 // not used
+#define BMI088_ACC_CS_PIN	PA4
+//#define BMI088_ACC_EXTI_PIN	PG8 // not used
 
 
 #define USE_IMU_BMI270
-#define IMU_BMI270_ALIGN        CW90_DEG_FLIP
-#define BMI270_CS_PIN           PF10
-#define BMI270_SPI_BUS          BUS_SPI1
+#define IMU_BMI270_ALIGN	CW90_DEG_FLIP
+#define BMI270_CS_PIN		PG8
+#define BMI270_SPI_BUS		BUS_SPI1
 
 
 // *************** Mag *****************************
 
 #define USE_I2C
 #define USE_I2C_DEVICE_2
-#define I2C2_SCL                PF1
-#define I2C2_SDA                PF0
+#define I2C2_SCL		PF1
+#define I2C2_SDA		PF0
 
-#define DEFAULT_I2C_BUS         BUS_I2C2
-#define EXTERNAL_I2C_BUS		BUS_I2C2
-#define MAG_I2C_BUS				BUS_I2C2
+#define DEFAULT_I2C_BUS		BUS_I2C2
+#define EXTERNAL_I2C_BUS	BUS_I2C2
+#define MAG_I2C_BUS		BUS_I2C2
 
+/* ERRATA: SCL and SDA mixed up on QMC's side
 #define USE_MAG
 #define USE_MAG_QMC5883
-#define QMC5883_I2C_BUS         BUS_I2C2
+#define QMC5883_I2C_BUS		BUS_I2C2
+*/
 #define USE_MAG_ALL
 
 // *************** Baro ****************************
@@ -74,64 +80,83 @@
 
 #define USE_BARO_BMP388
 #define BMP388_SPI_BUS			BUS_SPI1
-#define BMP388_CS_PIN			PF11
+#define BMP388_CS_PIN			PG10
 
 // *************** UART ****************************
 #define USE_VCP
 
 #define USE_UART1
-#define UART1_RX_PIN            PB7
-#define UART1_TX_PIN            PB6
+#define UART1_RX_PIN		PB7
+#define UART1_TX_PIN		PB6
 
 #define USE_UART2
-#define UART2_RX_PIN            PA3
-#define UART2_TX_PIN            PA2
+#define UART2_RX_PIN		PA3
+#define UART2_TX_PIN		PA2
 
 #define USE_UART3
-#define UART3_RX_PIN            PC11
-#define UART3_TX_PIN            PC10
+#define UART3_RX_PIN		PB11
+#define UART3_TX_PIN		PD8
 
+/* ERRATA: double TX pins PA0 and PC10
 #define USE_UART4
-#define UART4_RX_PIN            PA1
-#define UART4_TX_PIN            PA0
+#define UART4_RX_PIN		PA1
+#define UART4_TX_PIN		PA0
+*/
 
 #define USE_UART5
-#define UART5_RX_PIN            PD2
-#define UART5_TX_PIN            PC12
+#define UART5_RX_PIN		PD2
+#define UART5_TX_PIN		PC12
 
+/* ERRATA: NO TX pin ON PG15, PG14 should be used
 #define USE_UART6
-#define UART6_RX_PIN            PG9
-#define UART6_TX_PIN            PG14
+#define UART6_RX_PIN		PG9
+#define UART6_TX_PIN		PG14
+*/
 
-#define SERIAL_PORT_COUNT       7
+#define SERIAL_PORT_COUNT	5
 
 // *************** OSD *****************************
 
 #define USE_OSD
 #define USE_MAX7456
-#define MAX7456_SPI_BUS      BUS_SPI1
-#define MAX7456_CS_PIN       PA15
+#define MAX7456_SPI_BUS	BUS_SPI2
+#define MAX7456_CS_PIN	PF2
 
 // *************** ADC *****************************
 #define USE_ADC
-#define ADC_INSTANCE            ADC3
+#define ADC_INSTANCE			ADC3
 
-#define ADC_CHANNEL_1_PIN		PF5
-#define ADC_CHANNEL_2_PIN       PF4
+#define ADC_CHANNEL_1_PIN		PF6
+#define ADC_CHANNEL_2_PIN		PF5
 #define VBAT_ADC_CHANNEL		ADC_CHN_1
-#define CURRENT_METER_ADC_CHANNEL ADC_CHN_2
+#define CURRENT_METER_ADC_CHANNEL	ADC_CHN_2
 
 #define VBAT_SCALE_DEFAULT      1100
 #define CURRENT_METER_SCALE     5
 
-#define DEFAULT_FEATURES		(FEATURE_VBAT|FEATURE_TELEMETRY|FEATURE_CURRENT_METER/*|FEATURE_OSD*/)
+// ************** PINIO ****************************
+#define USE_PINIO
+#define USE_PINIOBOX
+#define PINIO1_PIN		PE0
+#define PINIO2_PIN		PE1
+#define PINIO3_PIN		PE2
+#define PINIO4_PIN		PE3
 
-#define TARGET_IO_PORTA         0xffff
-#define TARGET_IO_PORTB         0xffff
-#define TARGET_IO_PORTC         0xffff
-#define TARGET_IO_PORTD         0xffff
-#define TARGET_IO_PORTE         0xffff
-#define TARGET_IO_PORTF         0xffff
-#define TARGET_IO_PORTG         0xffff
+// ************** SDIO *****************************
+#define USE_SDCARD
+#define USE_SDCARD_SDIO
+#define SDCARD_SDIO_DMA		DMA_TAG(2,3,4)
+#define SDCARD_SDIO_4BIT
+#define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
+
+#define DEFAULT_FEATURES	(FEATURE_VBAT|FEATURE_TELEMETRY|FEATURE_CURRENT_METER|FEATURE_BLACKBOX/*|FEATURE_OSD*/)
+
+#define TARGET_IO_PORTA		0xffff
+#define TARGET_IO_PORTB		0xffff
+#define TARGET_IO_PORTC		0xffff
+#define TARGET_IO_PORTD		0xffff
+#define TARGET_IO_PORTE		0xffff
+#define TARGET_IO_PORTF		0xffff
+#define TARGET_IO_PORTG		0xffff
 
 #define MAX_PWM_OUTPUT_PORTS	13
