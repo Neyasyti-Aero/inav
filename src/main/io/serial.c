1bb62e6c6f1772d107f300dec29a68e9f723d564
DaLLuZZ <khrushiov2012@yandex.ru>
2025-02-26 13:57:16 +0300

    STRIZH target rework

--- src/main/io/serial.c
+++ src/main/io/serial.c
@@ -146,15 +146,10 @@ void pgResetFn_serialConfig(serialConfig_t *serialConfig)
 #ifdef USE_VCP
     if (serialConfig->portConfigs[0].identifier == SERIAL_PORT_USB_VCP) {
         serialPortConfig_t * uart1Config = serialFindPortConfiguration(SERIAL_PORT_USART1);
-        if (uart1Config) {
+        if (uart1Config && uart1Config->functionMask == 0) {
             uart1Config->functionMask = FUNCTION_MSP;
         }
     }
-#else
-	serialPortConfig_t * uart1Config = serialFindPortConfiguration(SERIAL_PORT_USART1);
-    if (uart1Config && uart1Config->functionMask == 0) {
-        uart1Config->functionMask = FUNCTION_MSP;
-	}
 #endif
 
     serialConfig->reboot_character = SETTING_REBOOT_CHARACTER_DEFAULT;
